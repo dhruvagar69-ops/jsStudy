a750697ddbc053def1d7318cd8d5f9eb4fc717b5
Dhruv Agarwal <dhruvagar69@gmail.com>
2026-01-05 11:39:00 +0000

    projects

---
+++ build-a-sentence-maker.js
@@ -0,0 +1,6 @@
+let noun = "squirrel", verb = "raging", adjective = "cute", adjective2 = "large", place = "tree", noun2 = "nuts";
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+noun = "crab", verb = "furious", place = "pond", adjective2 = "immense", noun2 = "insects", adjective = "large";
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
---
+++ lab-javascript-trivia-bot.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+//  The bot's name, where it's from, and its favorite coding language, respectively.
+const botName = 'Beep-Boop'
+const botLocation = 'Far Far Away'
+const favoriteLanguage = 'Rizz'
+
+console.log("My name is " + botName +" and I live on "+ botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+// First assignment and log
+let codingFact = 'beeeeeeeeep doooopppe??????? BBBBOOOOPPPPP!!!!!! ' + favoriteLanguage;
+console.log(codingFact);
+
+// Second assignment and log
+codingFact = favoriteLanguage + ' is the coolest programming language ever!';
+console.log(codingFact);
+
+// Third assignment and log
+codingFact = 'I absolutely love ' + favoriteLanguage + '!';
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".")
+
